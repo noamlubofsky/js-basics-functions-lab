e6804c5fb131c4e45f46b22872732f0c5ac832ea
github username <github email address>
2021-06-08 22:11:43 -0400

    commit tree 4f29e83af6200fa599f74de0e1f53c075fec0a92

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ function distanceFromHqInBlocks(pickup) {
     {return (hq - pickup)}
 }
 
-function distanceFromHqInFeet(blocks) {
-    distanceFromHqInBlocks(blocks)
+function distanceFromHqInFeet(pickup) {
+    return (distanceFromHqInBlocks(pickup) * feet)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return ((start - destination) * feet)
+    }
+    if (start < destination) {
+        return ((destination - start) * feet)
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) { 
+        return (0)
+    }
+    if (distanceTravelledInFeet(start, destination) < 2000) {
+        return ((distanceTravelledInFeet(start, destination)) - 400) * .02
+    }
+    if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return (25)
+    }
+    if (distanceTravelledInFeet(start, destination) > 2500) {
+        return ('cannot travel that far')
+    }
 }
