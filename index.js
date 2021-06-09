8eaf9dbf48055eb4903bb91ca822bc806db04ac3
github username <github email address>
2021-06-08 21:10:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const hq = 42
+const feet = 264
+
+function distanceFromHqInBlocks(pickup) {
+    if (pickup >= 43)  
+    {return (pickup - hq)}
+    if (pickup <= 42)
+    {return (hq - pickup)}
+}
+
+function distanceFromHqInFeet(distanceFromHqInBlocks) {
+    return (distanceFromHqInBlocks * feet)
+}
